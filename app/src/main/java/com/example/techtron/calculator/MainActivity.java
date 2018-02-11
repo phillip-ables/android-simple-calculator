c13c17b6054de2f0c761fbd5b094ea56f1588980
Phillip <phillipables1@gmail.com>
2018-02-11 00:32:36 -0500

    assign var to all the app widgets xml

--- app/src/main/java/com/example/techtron/calculator/MainActivity.java
+++ app/src/main/java/com/example/techtron/calculator/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.techtron.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //assign variables for each of those widget
+    private Button one, two, three, four, five, six, seven, eight, nine, zero;
+    private Button add, sub, div, mul;
+    private Button equal, clear;
+    private TextView info, result;
+    //associate each variables with thier ids in the xml file
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
