7c2da3ea342eb7a6025272bb07f41c3a53542a69
Phillip <phillipables1@gmail.com>
2018-02-11 00:43:40 -0500

    set up ui views

--- app/src/main/java/com/example/techtron/calculator/MainActivity.java
+++ app/src/main/java/com/example/techtron/calculator/MainActivity.java
@@ -10,11 +10,32 @@ public class MainActivity extends AppCompatActivity {
     private Button one, two, three, four, five, six, seven, eight, nine, zero;
     private Button add, sub, div, mul;
     private Button equal, clear;
-    private TextView info, result;
+    private TextView control, result;
     //associate each variables with thier ids in the xml file
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setupUIViews() {
+        zero = (Button)findViewById(R.id.btn0);
+        one = (Button)findViewById(R.id.btn1);
+        two = (Button)findViewById(R.id.btn2);
+        three = (Button)findViewById(R.id.btn3);
+        four = (Button)findViewById(R.id.btn4);
+        five  = (Button)findViewById(R.id.btn5);
+        six = (Button)findViewById(R.id.btn6);
+        seven = (Button)findViewById(R.id.btn7);
+        eight = (Button)findViewById(R.id.btn8);
+        nine = (Button)findViewById(R.id.btn9);
+        add = (Button)findViewById(R.id.btnadd);
+        sub = (Button)findViewById(R.id.btnsub);
+        div = (Button)findViewById(R.id.btndiv);
+        mul = (Button)findViewById(R.id.btnmul);
+        equal = (Button)findViewById(R.id.btnequal);
+        clear = (Button)findViewById(R.id.btnclear);
+        control = (TextView)findViewById(R.id.tvControl);
+        result = (TextView)findViewById(R.id.tvResults);
     }
 }
