76b829770c95b794537c1734138fd99fa4aa79aa
Phillip <phillipables1@gmail.com>
2018-02-11 01:08:54 -0500

    set OnClickListeners to digit buttons that concat the info textView to update current with new digit input

--- app/src/main/java/com/example/techtron/calculator/MainActivity.java
+++ app/src/main/java/com/example/techtron/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.techtron.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,12 +11,75 @@ public class MainActivity extends AppCompatActivity {
     private Button one, two, three, four, five, six, seven, eight, nine, zero;
     private Button add, sub, div, mul;
     private Button equal, clear;
-    private TextView control, result;
+    private TextView info, result;
     //associate each variables with thier ids in the xml file
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUIViews();
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "0");
+            }
+        });
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "1");
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "8")
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                info.setText(info.getText().toString() + "9");
+            }
+        });
     }
 
     private void setupUIViews() {
@@ -35,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         mul = (Button)findViewById(R.id.btnmul);
         equal = (Button)findViewById(R.id.btnequal);
         clear = (Button)findViewById(R.id.btnclear);
-        control = (TextView)findViewById(R.id.tvControl);
+        info = (TextView)findViewById(R.id.tvControl);
         result = (TextView)findViewById(R.id.tvResults);
     }
 }
